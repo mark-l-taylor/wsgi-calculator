af5032183b5892121c4ed8d33a6a5d1643bdbdd7
Mark L Taylor <taylormarkld@gmail.com>
2021-04-02 15:25:07 -0700

    completed lesson 04 assignment

--- calculator.py
+++ calculator.py
@@ -40,6 +40,22 @@ To submit your homework:
 
 
 """
+import traceback
+
+
+def home(*args):
+    page = """
+<h1>Simple Calculator</h1>
+<p>Perform basic calculations by entering method/value1/value2 in the address.</p>
+<h3>Examples:</h3> 
+<ul>
+    <li>add: /add/3/1</li>
+    <li>subtract: /subtract/3/1</li>
+    <li>multiply: /multiply/3/1</li>
+    <li>divide: /divide/3/1</li>
+</ul>
+"""
+    return page
 
 
 def add(*args):
@@ -47,11 +63,35 @@ def add(*args):
 
     # TODO: Fill sum with the correct value, based on the
     # args provided.
-    sum = "0"
 
-    return sum
+    try:
+        return str(sum(map(int, args)))
+    except (ValueError, TypeError):
+        return "Unable to calculate: please provide integer values."
 
-# TODO: Add functions for handling more arithmetic operations.
+
+def subtract(*args):
+    try:
+        return str(int(args[0]) - int(args[1]))
+    except (ValueError, TypeError):
+        raise TypeError
+
+
+def multiply(*args):
+    try:
+        return str(int(args[0]) * int(args[1]))
+    except (ValueError, TypeError):
+        raise TypeError
+
+
+def divide(*args):
+    try:
+        return str(int(args[0]) / int(args[1]))
+    except (ValueError, TypeError):
+        raise TypeError
+    except ZeroDivisionError:
+        raise ZeroDivisionError
+
 
 def resolve_path(path):
     """
@@ -59,26 +99,58 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '': home,
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide,
+    }
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
+    return func, args
+
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found. Please enter, add, subtract, multiply or divide</h1>"
+    except (ValueError, TypeError):
+        status = "400 Bad Request"
+        body = "<h1>Unable to calculate: please provide integer values.</h1>"
+    except ZeroDivisionError:
+        status = "400 Bad Request"
+        body = "<h1>Error: Division by zero</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
